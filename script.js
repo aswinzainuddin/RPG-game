3246c8f33f46beb398567ae6b6d264138f650fdd
Muhammad Aswin Zainuddin <azwinmuhammad4@gmail.com>
2025-02-15 16:40:57 +0700

    Update script.js add random name function

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
-// const nameInput = document.querySelector('#submit-name');
-// const confirmationName = confirm(`is ${nameInput} your name???`);
+document.addEventListener('DOMContentLoaded', () => {
+    const randomNameBtn = document.getElementById("random-name-button");
+    const playerName = document.getElementById("player-name");
+    const submitName = document.getElementById("submit-name")
+    const randomNames = ["Eldrin", "Thalor", "Seraphina", "Garrik", "Vexaria", "Drogan", "Liora", "Pasta Sempa"];
 
-
-const PlayerNameOptions = [
-    "John Smith", "Dave Maerk", "Max Joe", "Martin Sempa"
-];
-function getRandomPlayerName() {
-
-}
-
-// button initialization
-const randomNameButton = document.querySelector('#random-name');
-randomNameButton.onclick = ;
+    randomNameBtn.addEventListener('click', () => {
+        const randomIndex = Math.floor(Math.random() * randomNames.length);
+        playerName.value = randomNames[randomIndex];
+    })
+    
+    submitName.addEventListener('submit', (e) => {
+        e.preventDefault();
+        confirm(`So you are ${playerName.value}, aren't you?`);
+    })
+})

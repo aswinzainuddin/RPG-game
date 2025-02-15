62208fa2685767e2c35cefa5220032e1da3f40eb
aswinzainuddin <azwinmuhammad4@gmail.com>
2025-02-15 14:51:31 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+// const nameInput = document.querySelector('#submit-name');
+// const confirmationName = confirm(`is ${nameInput} your name???`);
+
+
+const PlayerNameOptions = [
+    "John Smith", "Dave Maerk", "Max Joe", "Martin Sempa"
+];
+function getRandomPlayerName() {
+
+}
+
+// button initialization
+const randomNameButton = document.querySelector('#random-name');
+randomNameButton.onclick = ;
